49d4297f3d70a863be3e09790be0ddc7cf22ba66
Jason Hsu <jhsu802701@jasonhsu.com>
2019-06-03 15:25:19 +0000

    Added outlining gems and scripts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,9 @@
 Rails.application.configure do
+  # BEGIN: mailcatcher configuration
+  # From http://mailcatcher.me/
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+  # END: mailcatcher configuration
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
