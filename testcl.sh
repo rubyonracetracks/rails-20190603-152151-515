9166fd4bef2f5bba2d310bb4a6316de9ec80beb2
Jason Hsu <jhsu802701@jasonhsu.com>
2019-06-03 15:23:53 +0000

    Added quick testing scripts

---
+++ testcl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This is a slightly longer version of testc.sh.
+
+echo '---------------------------------------'
+echo 'bundle exec brakeman -Aq -w2 --no-pager'
+bundle exec brakeman -Aq -w2 --no-pager
+
+echo '----------------------'
+echo 'bundle exec rubocop -D'
+bundle exec rubocop -D
+
+echo '----------------------------------'
+echo 'bundle exec rails_best_practices .'
+bundle exec rails_best_practices .
+
+sh testc.sh
