15f8db4929196e7229a68cdc94cd66ca2a674db4
Jason Hsu <jhsu802701@jasonhsu.com>
2019-06-03 15:25:38 +0000

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,14 @@
 #
 module ApplicationHelper
+  # BEGIN: full_title
+  # Returns the full title on a per-page basis
+  def full_title(page_title = '')
+    base_title = 'Generic App Template'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
+  # END: full_title
 end
